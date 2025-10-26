d43f8cfa883e305a39590f30943fe0e241c562a1
Jana Nekvapilová <jana.nekva@gmail.com>
2025-10-26 19:11:46 +0100

    add avatar placeholder and update styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ export const App = () => {
   return (
     <div className="container">
       <h1>REGISTRATION</h1>
+      <div className="avatar-container">
+        <img src="/user.png" alt="avatar" />
+      </div>
       <Registration/>
     </div>
   );
